42575e23f1ce1499d465aab833f5f325d2539ac3
nikit <nikita_eremeev.95@mail.ru>
2024-09-02 16:18:27 +0300

    added RegistrationScreen

--- app/src/main/java/com/kaz4/composemessanger/MainActivity.kt
+++ app/src/main/java/com/kaz4/composemessanger/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.kaz4.composemessanger.ui.auth.screens.RegistrationScreen
 import com.kaz4.composemessanger.ui.theme.ComposeMessangerTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeMessangerTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+//                    AuthorizationScreen(
+//                        onPhoneNumberSubmit = { phoneNumber ->
+//                            // Логика отправки номера телефона
+//                            // Например, отправка запроса на сервер
+//                        },
+//                        onCountryCodeSubmit = { countryCode ->
+//                            // Логика отправки номера телефона
+//                            // Например, отправка запроса на сервер
+//                        },
+//                        onCodeSubmit = { code ->
+//                            // Логика отправки кода подтверждения
+//                            // Например, отправка запроса на сервер
+//                        },
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    RegistrationScreen(
+                        phoneNumber = "+7 (999) 999-99-99",
+                        onUsernameChange = {
+                            
+                        },
+                        onRegister = { phoneNumber, username ->
 
+                        },
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
